128cf01ed0c2061e6988c270ec1b1b95cfd1d60b
xiaozhang <1925165292@qq.com>
2022-02-16 12:29:14 +0800

    [zouyuejiuchu/Cpractice] some optimize

--- thread_pool.h
+++ thread_pool.h
@@ -60,7 +60,7 @@ private:
     std::mutex task_mutex;// m_mutex是为了配合have_task_cond来通知任务队列里有任务
     std::mutex queue_empty_mutex;// mutex2是为了锁住队列里没有任务的状态。
     std::mutex add_mutex; // add_mutex为了锁住任务队列，防止继续添加任务
-    std::condition_variable have_task_cond; // 
+    std::condition_variable have_task_cond; // 通知有任务的到来。
     std::condition_variable queue_empty_cond; // 为了通知队列为空状态的出现
 };
 
@@ -73,21 +73,18 @@ void thread_pool::start() {
 }
 
 void thread_pool::stop_until_empty() {
-    std::unique_lock<std::mutex> lock1(queue_empty_mutex); // 首先需要获得任务队列没有任务的状态
+    std::unique_lock<std::mutex> lock1(queue_empty_mutex); // 获得锁
     std::unique_lock<std::mutex> lock2(add_mutex); // 获得任务队列的状态，防止继续添加任务
     if (!Tasks.empty()) { // 如果任务队列不空，那么就需要等待，直到信号量queue_empty_cond可用
-        queue_empty_cond.wait(lock1); // 需要释放queue_empty_mutex
+        queue_empty_cond.wait(lock1); // wait的同时会释放queue_empty_mutex
     }
     stop();
     return ;
 }
 
 void thread_pool::stop() {
-    {
-        std::unique_lock<std::mutex> lock(is_started_mutex);
-        is_started = false;
-        have_task_cond.notify_all();
-    }
+    std::unique_lock<std::mutex> lock(is_started_mutex);
+    is_started = false;
     for (int i = 0; i < Threads.size(); i++) {
         Threads[i]->join();
         delete Threads[i];
